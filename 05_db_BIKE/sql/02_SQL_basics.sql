3a55cd3c9290d90a29bcd9360d74abc456419945
Peter <schmidt.multimedia@gmail.com>
2022-12-16 10:02:15 +0100

    db BIKE

---
+++ 05_db_BIKE/sql/02_SQL_basics.sql
@@ -0,0 +1,30 @@
+SHOW DATABASES;
+USE bike;
+SHOW TABLES;
+
+DESCRIBE Artikel;
+SELECT * FROM Artikel;
+
+DESCRIBE Auftrag;
+SELECT * FROM Auftrag;
+
+DESCRIBE Auftragsposten;
+SELECT * FROM Auftragsposten;
+
+DESCRIBE Kunde;
+SELECT * FROM Kunde;
+
+DESCRIBE Lager;
+SELECT * FROM Lager;
+
+DESCRIBE Lieferant;
+SELECT * FROM Lieferant;
+
+DESCRIBE Lieferung;
+SELECT * FROM Lieferung;
+
+DESCRIBE Personal;
+SELECT * FROM Personal;
+
+DESCRIBE Teilestruktur;
+SELECT * FROM Teilestruktur;
